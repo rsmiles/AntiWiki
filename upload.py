e9968a9405abf914c44697eb80b70a6e8d203f50
Robert Smiley <yarnoiser@gmail.com>
2019-12-03 20:33:30 +0000

    Corrections to file uploading

--- upload.py
+++ upload.py
@@ -2,26 +2,29 @@
 
 import cgi, cgitb, os
 
-cgit.enable()
-
+cgitb.enable()
 form = cgi.FieldStorage()
+
+result = ''
+
 fileitem=form['filename']
 if fileitem.filename:
     filename = os.path.basename(fileitem.filename.replace('\\', '/'))
     with open('/tmp/' + filename, 'wb') as f:
         f.write(fileitem.file.read())
-
-    result = 'File "' + filename + ' uploaded'
+    result = 'File "' + filename + '" uploaded'
 else:
     result = 'No file was provided'
 
 output = """\
+HTTP/1.0 200 OK
 Content-Type: text/html
+
 <html>
 <body>
     <p>{}</p>
 </body>
-</html>
+</html>\
 """.format(result)
 
 print(output)
